9ba1819b2c2b4f8f40d06c21b5e9202e9617802b
alumno <sebastian.witonski@gmail.com>
2025-10-14 11:51:49 +0200

    commit

---
+++ src/Ejercicio3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        int[] numEnteros = {1, 3, 9, 10, 12};
+        int posiciones = 0;
+        int num = 0;
+
+    }
+}
+
+
+
+
+
+
