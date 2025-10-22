429584fa3dd70f9994ac9bf6fb9d69fe4a07d97e
Sebastian <sebastian.witonski@gmail.com>
2025-10-22 16:11:43 +0200

    commit

--- src/Boletin_Arrays/Ejercicio5.java
+++ src/Boletin_Arrays/Ejercicio5.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Ejercicio5 {
+/*
+La línea public static void generarYMostrarArray(int tamano) es la declaración completa del metodo (o función)
+ que realiza una tarea específica en el código. La tarea principal de este metodo es triple:
+ Generar: Crear un array de enteros del tamaño (tamano) especificado.
+ Rellenar: Llenar cada posición de ese array con un número entero aleatorio entre 0 y 1000.
+ Mostrar: Imprimir el contenido final del array en la consola.
+ */
 
     public static void generarYMostrarArray(int tamano){
 
@@ -23,6 +30,7 @@ public class Ejercicio5 {
         System.out.println("Array generado con " + tamano + " posiciones:");
         System.out.println(Arrays.toString(arrayAleatorio));
     }
+
     public static void main(String[] args) {
         // Ejemplos de uso del método:
         System.out.println("--- Ejemplo 1: Tamaño 12 ---");
