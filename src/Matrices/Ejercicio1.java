56dad6d8be44e485a1b520c3b4163f6264e1dc95
Sebastian <sebastian.witonski@gmail.com>
2025-10-23 17:23:15 +0200

    commit

---
+++ src/Matrices/Ejercicio1.java
@@ -0,0 +1,32 @@
+package Matrices;
+/*
+1. Crea un metodo que compruebe si en una matriz hay algún valor negativo, y devuelva un
+valor booleano.
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // 1. Crear una matriz de prueba
+        int[][] matriz = {
+                {10, 20, 30},
+                {40, -5, 60},
+                {70, 80, -90}
+        };
+
+
+        System.out.println("¿La Matriz  contiene negativos? " + contieneNegativos(matriz));
+
+    }
+
+     public static boolean contieneNegativos(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] < 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+
