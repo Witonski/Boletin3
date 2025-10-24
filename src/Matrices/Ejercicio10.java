524e897128870840ebc2d8a497eea903e9c243bf
alumno <sebastian.witonski@gmail.com>
2025-10-24 10:13:59 +0200

    commit

---
+++ src/Matrices/Ejercicio10.java
@@ -0,0 +1,17 @@
+package Matrices;
+/*
+10. Crea un metodo que compruebe si una matriz pasada por parámetro es simétrica en
+función del eje Y. Es decir, la matriz
+1 3 0 3 1
+4 2 7 2 4
+6 1 9 1 6
+0 8 8 8 0
+es simétrica en función de su eje Y, porque si invirtiésemos el orden en el que leemos las
+columnas, la matriz se vería igual.
+Pero la matriz
+1 4 6
+2 1 3
+no es simétrica, pues no cumple dicha propiedad.
+ */
+public class Ejercicio10 {
+}
--- src/Matrices/Ejercicio9.java
+++ src/Matrices/Ejercicio9.java
@@ -6,6 +6,12 @@ función del eje X. Es decir, la matriz
 4 2 0 8
 4 2 0 8
 1 3 5 6
+es simétrica en función de su eje X, porque si invirtiésemos el orden en el que leemos las
+filas, la matriz se vería igual.
+Pero la matriz
+1 4 6
+2 1 3
+no es simétrica, pues no cumple dicha propiedad.
  */
 public class Ejercicio9 {
 }
